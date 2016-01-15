25ffeeba7ca18efca9caa9d923b5a7693a85c047
Flavio Martins <flaviomartins@acm.org>
2016-01-15 01:55:30 +0000

    Update query processor

--- src/main/java/at/ac/univie/mminf/luceneSKOS/queryparser/flexible/standard/processors/SKOSQueryNodeProcessor.java
+++ src/main/java/at/ac/univie/mminf/luceneSKOS/queryparser/flexible/standard/processors/SKOSQueryNodeProcessor.java
@@ -17,7 +17,9 @@ package at.ac.univie.mminf.luceneSKOS.queryparser.flexible.standard.processors;
  */
 
 import java.io.IOException;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -29,10 +31,13 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
 import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
 import org.apache.lucene.queryparser.flexible.core.config.QueryConfigHandler;
+import org.apache.lucene.queryparser.flexible.core.nodes.BooleanQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.BoostQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.FieldQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.FuzzyQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.GroupQueryNode;
+import org.apache.lucene.queryparser.flexible.core.nodes.ModifierQueryNode;
+import org.apache.lucene.queryparser.flexible.core.nodes.ModifierQueryNode.Modifier;
 import org.apache.lucene.queryparser.flexible.core.nodes.NoTokenFoundQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.QueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.QuotedFieldQueryNode;
@@ -41,6 +46,7 @@ import org.apache.lucene.queryparser.flexible.core.nodes.TextableQueryNode;
 import org.apache.lucene.queryparser.flexible.core.nodes.TokenizedPhraseQueryNode;
 import org.apache.lucene.queryparser.flexible.core.processors.QueryNodeProcessorImpl;
 import org.apache.lucene.queryparser.flexible.standard.config.StandardQueryConfigHandler.ConfigurationKeys;
+import org.apache.lucene.queryparser.flexible.standard.config.StandardQueryConfigHandler.Operator;
 import org.apache.lucene.queryparser.flexible.standard.nodes.MultiPhraseQueryNode;
 import org.apache.lucene.queryparser.flexible.standard.nodes.RegexpQueryNode;
 import org.apache.lucene.queryparser.flexible.standard.nodes.StandardBooleanQueryNode;
@@ -55,17 +61,17 @@ import at.ac.univie.mminf.luceneSKOS.analysis.SKOSTypeAttribute.SKOSType;
  * not <code>null</code>, it looks for every {@link FieldQueryNode} that is not
  * {@link WildcardQueryNode}, {@link FuzzyQueryNode} or
  * {@link RangeQueryNode} contained in the query node tree, then it applies
- * the analyzer to that {@link FieldQueryNode} object. <br/>
- * <br/>
+ * the analyzer to that {@link FieldQueryNode} object. <br>
+ * <br>
  * If the analyzer return only one term, the returned term is set to the
- * {@link FieldQueryNode} and it's returned. <br/>
- * <br/>
+ * {@link FieldQueryNode} and it's returned. <br>
+ * <br>
  * If the analyzer return more than one term, a {@link TokenizedPhraseQueryNode}
  * or {@link MultiPhraseQueryNode} is created, whether there is one or more
- * terms at the same position, and it's returned. <br/>
- * <br/>
+ * terms at the same position, and it's returned. <br>
+ * <br>
  * If no term is returned by the analyzer a {@link NoTokenFoundQueryNode} object
- * is returned. <br/>
+ * is returned.
  * 
  * @see ConfigurationKeys#ANALYZER
  * @see Analyzer
@@ -76,6 +82,8 @@ public class SKOSQueryNodeProcessor extends QueryNodeProcessorImpl {
   private Analyzer analyzer;
 
   private boolean positionIncrementsEnabled;
+  
+  private Operator defaultOperator;
 
   private Map<SKOSType,Float> boosts;
 
@@ -88,7 +96,9 @@ public class SKOSQueryNodeProcessor extends QueryNodeProcessorImpl {
     if (analyzer != null) {
       this.positionIncrementsEnabled = false;
       Boolean positionIncrementsEnabled = getQueryConfigHandler().get(ConfigurationKeys.ENABLE_POSITION_INCREMENTS);
-
+      Operator defaultOperator = getQueryConfigHandler().get(ConfigurationKeys.DEFAULT_OPERATOR);
+      this.defaultOperator = defaultOperator != null ? defaultOperator : Operator.OR;
+      
       if (positionIncrementsEnabled != null) {
           this.positionIncrementsEnabled = positionIncrementsEnabled;
       }
@@ -96,7 +106,6 @@ public class SKOSQueryNodeProcessor extends QueryNodeProcessorImpl {
       if (this.analyzer != null) {
         return super.process(queryTree);
       }
-
     }
 
     return queryTree;
@@ -116,220 +125,280 @@ public class SKOSQueryNodeProcessor extends QueryNodeProcessorImpl {
       String text = fieldNode.getTextAsString();
       String field = fieldNode.getFieldAsString();
 
-      TokenStream source;
-      try {
-        source = this.analyzer.tokenStream(field, text);
-        source.reset();
-      } catch (IOException e1) {
-        throw new RuntimeException(e1);
-      }
-      CachingTokenFilter buffer = new CachingTokenFilter(source);
-
+      CachingTokenFilter buffer = null;
       PositionIncrementAttribute posIncrAtt = null;
       int numTokens = 0;
       int positionCount = 0;
       boolean severalTokensAtSamePosition = false;
-
-      if (buffer.hasAttribute(PositionIncrementAttribute.class)) {
-        posIncrAtt = buffer.getAttribute(PositionIncrementAttribute.class);
-      }
-
+      
       try {
+        try (TokenStream source = this.analyzer.tokenStream(field, text)) {
+          buffer = new CachingTokenFilter(source);
+          buffer.reset();
 
-        while (buffer.incrementToken()) {
-          numTokens++;
-          int positionIncrement = (posIncrAtt != null) ? posIncrAtt
-              .getPositionIncrement() : 1;
-          if (positionIncrement != 0) {
-            positionCount += positionIncrement;
-
-          } else {
-            severalTokensAtSamePosition = true;
+          if (buffer.hasAttribute(PositionIncrementAttribute.class)) {
+            posIncrAtt = buffer.getAttribute(PositionIncrementAttribute.class);
           }
-
-        }
-
-      } catch (IOException e) {
-        // ignore
-      }
-
-      try {
-        // rewind the buffer stream
-        buffer.reset();
-
-        // close original stream - all tokens buffered
-        source.close();
-      } catch (IOException e) {
-        // ignore
-      }
-
-      if (!buffer.hasAttribute(CharTermAttribute.class)) {
-        return new NoTokenFoundQueryNode();
-      }
-
-      CharTermAttribute termAtt = buffer.getAttribute(CharTermAttribute.class);
-
-      if (numTokens == 0) {
-        return new NoTokenFoundQueryNode();
-
-      } else if (numTokens == 1) {
-        String term = null;
-        try {
-          boolean hasNext;
-          hasNext = buffer.incrementToken();
-          assert hasNext == true;
-          term = termAtt.toString();
-
-        } catch (IOException e) {
-          // safe to ignore, because we know the number of tokens
-        }
-
-        fieldNode.setText(term);
-
-        return fieldNode;
-
-      } else if (severalTokensAtSamePosition || !(node instanceof QuotedFieldQueryNode)) {
-        if (positionCount == 1 || !(node instanceof QuotedFieldQueryNode)) {
-          // no phrase query:
-          LinkedList<QueryNode> children = new LinkedList<QueryNode>();
-
-          for (int i = 0; i < numTokens; i++) {
-            String term = null;
-            try {
-              boolean hasNext = buffer.incrementToken();
-              assert hasNext == true;
-              term = termAtt.toString();
-
-            } catch (IOException e) {
-              // safe to ignore, because we know the number of tokens
-            }
-            
-            if (buffer.hasAttribute(SKOSTypeAttribute.class) && boosts != null) {
-
-              SKOSTypeAttribute skosAttr = buffer.getAttribute(SKOSTypeAttribute.class);
-
-              children.add(new BoostQueryNode(new FieldQueryNode(field, term, -1, -1), getBoost(skosAttr.getSkosType())));
-
-            } else {
-
-              children.add(new FieldQueryNode(field, term, -1, -1));
-
-            }
-
-          }
-          return new GroupQueryNode(
-            new StandardBooleanQueryNode(children, positionCount==1));
-        } else {
-          // phrase query:
-          MultiPhraseQueryNode mpq = new MultiPhraseQueryNode();
-
-          List<FieldQueryNode> multiTerms = new ArrayList<FieldQueryNode>();
-          int position = -1;
-          int i = 0;
-          int termGroupCount = 0;
-          for (; i < numTokens; i++) {
-            String term = null;
-            int positionIncrement = 1;
-            try {
-              boolean hasNext = buffer.incrementToken();
-              assert hasNext == true;
-              term = termAtt.toString();
-              if (posIncrAtt != null) {
-                positionIncrement = posIncrAtt.getPositionIncrement();
-              }
-
-            } catch (IOException e) {
-              // safe to ignore, because we know the number of tokens
-            }
-
-            if (positionIncrement > 0 && multiTerms.size() > 0) {
-
-              for (FieldQueryNode termNode : multiTerms) {
-
-                if (this.positionIncrementsEnabled) {
-                  termNode.setPositionIncrement(position);
-                } else {
-                  termNode.setPositionIncrement(termGroupCount);
-                }
-
-                mpq.add(termNode);
-
-              }
-
-              // Only increment once for each "group" of
-              // terms that were in the same position:
-              termGroupCount++;
-
-              multiTerms.clear();
-
-            }
-
-            position += positionIncrement;
-            multiTerms.add(new FieldQueryNode(field, term, -1, -1));
-
-          }
-
-          for (FieldQueryNode termNode : multiTerms) {
-
-            if (this.positionIncrementsEnabled) {
-              termNode.setPositionIncrement(position);
-
-            } else {
-              termNode.setPositionIncrement(termGroupCount);
-            }
-
-            mpq.add(termNode);
-
-          }
-
-          return mpq;
-
-        }
-
-      } else {
-
-        TokenizedPhraseQueryNode pq = new TokenizedPhraseQueryNode();
-
-        int position = -1;
-
-        for (int i = 0; i < numTokens; i++) {
-          String term = null;
-          int positionIncrement = 1;
-
+  
           try {
-            boolean hasNext = buffer.incrementToken();
+  
+            while (buffer.incrementToken()) {
+              numTokens++;
+              int positionIncrement = (posIncrAtt != null) ? posIncrAtt
+                  .getPositionIncrement() : 1;
+              if (positionIncrement != 0) {
+                positionCount += positionIncrement;
+  
+              } else {
+                severalTokensAtSamePosition = true;
+              }
+  
+            }
+  
+          } catch (IOException e) {
+            // ignore
+          }
+
+          // rewind the buffer stream
+          buffer.reset();//will never through on subsequent reset calls
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+
+        if (!buffer.hasAttribute(CharTermAttribute.class)) {
+          return new NoTokenFoundQueryNode();
+        }
+  
+        CharTermAttribute termAtt = buffer.getAttribute(CharTermAttribute.class);
+  
+        if (numTokens == 0) {
+          return new NoTokenFoundQueryNode();
+  
+        } else if (numTokens == 1) {
+          String term = null;
+          try {
+            boolean hasNext;
+            hasNext = buffer.incrementToken();
             assert hasNext == true;
             term = termAtt.toString();
-
-            if (posIncrAtt != null) {
-              positionIncrement = posIncrAtt.getPositionIncrement();
-            }
-
+  
           } catch (IOException e) {
             // safe to ignore, because we know the number of tokens
           }
+  
+          fieldNode.setText(term);
+  
+          return fieldNode;
+  
+        } else if (severalTokensAtSamePosition || !(node instanceof QuotedFieldQueryNode)) {
+          if (positionCount == 1 || !(node instanceof QuotedFieldQueryNode)) {
+            // no phrase query:
+            
+            if (positionCount == 1) {
+              // simple case: only one position, with synonyms
+              LinkedList<QueryNode> children = new LinkedList<>();
+              
+              for (int i = 0; i < numTokens; i++) {
+                String term = null;
+                try {
+                  boolean hasNext = buffer.incrementToken();
+                  assert hasNext == true;
+                  term = termAtt.toString();
+                  
+                } catch (IOException e) {
+                  // safe to ignore, because we know the number of tokens
+                }
 
-          FieldQueryNode newFieldNode = new FieldQueryNode(field, term, -1, -1);
+                if (buffer.hasAttribute(SKOSTypeAttribute.class) && boosts != null) {
 
-          if (this.positionIncrementsEnabled) {
-            position += positionIncrement;
-            newFieldNode.setPositionIncrement(position);
+                  SKOSTypeAttribute skosAttr = buffer.getAttribute(SKOSTypeAttribute.class);
 
+                  children.add(new BoostQueryNode(new FieldQueryNode(field, term, -1, -1), getBoost(skosAttr.getSkosType())));
+
+                } else {
+
+                  children.add(new FieldQueryNode(field, term, -1, -1));
+
+                }
+                
+              }
+              return new GroupQueryNode(
+                  new StandardBooleanQueryNode(children, positionCount==1));
+            } else {
+              // multiple positions
+              QueryNode q = new StandardBooleanQueryNode(Collections.<QueryNode>emptyList(),false);
+              QueryNode currentQuery = null;
+              for (int i = 0; i < numTokens; i++) {
+                String term = null;
+                try {
+                  boolean hasNext = buffer.incrementToken();
+                  assert hasNext == true;
+                  term = termAtt.toString();
+                } catch (IOException e) {
+                  // safe to ignore, because we know the number of tokens
+                }
+                if (posIncrAtt != null && posIncrAtt.getPositionIncrement() == 0) {
+                  if (!(currentQuery instanceof BooleanQueryNode)) {
+                    QueryNode t = currentQuery;
+                    currentQuery = new StandardBooleanQueryNode(Collections.<QueryNode>emptyList(), true);
+                    ((BooleanQueryNode)currentQuery).add(t);
+                  }
+                  ((BooleanQueryNode)currentQuery).add(new FieldQueryNode(field, term, -1, -1));
+                } else {
+                  if (currentQuery != null) {
+                    if (this.defaultOperator == Operator.OR) {
+                      q.add(currentQuery);
+                    } else {
+                      q.add(new ModifierQueryNode(currentQuery, Modifier.MOD_REQ));
+                    }
+                  }
+                  currentQuery = new FieldQueryNode(field, term, -1, -1);
+                }
+              }
+              if (this.defaultOperator == Operator.OR) {
+                q.add(currentQuery);
+              } else {
+                q.add(new ModifierQueryNode(currentQuery, Modifier.MOD_REQ));
+              }
+              
+              if (q instanceof BooleanQueryNode) {
+                q = new GroupQueryNode(q);
+              }
+              return q;
+            }
           } else {
-            newFieldNode.setPositionIncrement(i);
+            // phrase query:
+            MultiPhraseQueryNode mpq = new MultiPhraseQueryNode();
+  
+            List<FieldQueryNode> multiTerms = new ArrayList<>();
+            int position = -1;
+            int i = 0;
+            int termGroupCount = 0;
+            for (; i < numTokens; i++) {
+              String term = null;
+              int positionIncrement = 1;
+              try {
+                boolean hasNext = buffer.incrementToken();
+                assert hasNext == true;
+                term = termAtt.toString();
+                if (posIncrAtt != null) {
+                  positionIncrement = posIncrAtt.getPositionIncrement();
+                }
+  
+              } catch (IOException e) {
+                // safe to ignore, because we know the number of tokens
+              }
+  
+              if (positionIncrement > 0 && multiTerms.size() > 0) {
+  
+                for (FieldQueryNode termNode : multiTerms) {
+  
+                  if (this.positionIncrementsEnabled) {
+                    termNode.setPositionIncrement(position);
+                  } else {
+                    termNode.setPositionIncrement(termGroupCount);
+                  }
+  
+                  mpq.add(termNode);
+  
+                }
+  
+                // Only increment once for each "group" of
+                // terms that were in the same position:
+                termGroupCount++;
+  
+                multiTerms.clear();
+  
+              }
+  
+              position += positionIncrement;
+              multiTerms.add(new FieldQueryNode(field, term, -1, -1));
+  
+            }
+  
+            for (FieldQueryNode termNode : multiTerms) {
+  
+              if (this.positionIncrementsEnabled) {
+                termNode.setPositionIncrement(position);
+  
+              } else {
+                termNode.setPositionIncrement(termGroupCount);
+              }
+  
+              mpq.add(termNode);
+  
+            }
+  
+            return mpq;
+  
           }
-
-          pq.add(newFieldNode);
-
+  
+        } else {
+  
+          TokenizedPhraseQueryNode pq = new TokenizedPhraseQueryNode();
+  
+          int position = -1;
+  
+          for (int i = 0; i < numTokens; i++) {
+            String term = null;
+            int positionIncrement = 1;
+  
+            try {
+              boolean hasNext = buffer.incrementToken();
+              assert hasNext == true;
+              term = termAtt.toString();
+  
+              if (posIncrAtt != null) {
+                positionIncrement = posIncrAtt.getPositionIncrement();
+              }
+  
+            } catch (IOException e) {
+              // safe to ignore, because we know the number of tokens
+            }
+  
+            FieldQueryNode newFieldNode = new FieldQueryNode(field, term, -1, -1);
+  
+            if (this.positionIncrementsEnabled) {
+              position += positionIncrement;
+              newFieldNode.setPositionIncrement(position);
+  
+            } else {
+              newFieldNode.setPositionIncrement(i);
+            }
+  
+            pq.add(newFieldNode);
+  
+          }
+  
+          return pq;
+  
         }
-
-        return pq;
-
+      } finally {
+        if (buffer != null) {
+          try {
+            buffer.close();
+          } catch (IOException e) {
+            // safe to ignore
+          }
+        }
       }
-
     }
 
     return node;
+  }
+
+  @Override
+  protected QueryNode preProcessNode(QueryNode node) throws QueryNodeException {
+
+    return node;
+
+  }
+
+  @Override
+  protected List<QueryNode> setChildrenOrder(List<QueryNode> children)
+      throws QueryNodeException {
+
+    return children;
 
   }
 
@@ -357,21 +426,6 @@ public class SKOSQueryNodeProcessor extends QueryNodeProcessorImpl {
     }
     
     return 1;
-  }
-
-  @Override
-  protected QueryNode preProcessNode(QueryNode node) throws QueryNodeException {
-
-    return node;
-
-  }
-
-  @Override
-  protected List<QueryNode> setChildrenOrder(List<QueryNode> children)
-      throws QueryNodeException {
-
-    return children;
-
   }
 
 }
